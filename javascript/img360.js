1d0a696c6b327725754ab2e36935155a00f27504
Ken30116 <ken141718@gmail.com>
2021-10-04 00:08:30 +0800

    壓縮檔案

--- javascript/img360.js
+++ javascript/img360.js
@@ -1,9 +1,1 @@
-$(function () {
-    $('.imgbox').mouseover(
-        function pop() {
-        let p=Math.ceil(Math.random()*4)*90 //亂數0~4,ceil=>1,2,3,4=>90,180,270,360
-        $(this).css({
-            transform: `rotate(${p}deg)`
-        });
-    });
-});
+"use strict";$(function(){$(".imgbox").mouseover(function(){var t=90*Math.ceil(4*Math.random());$(this).css({transform:"rotate(".concat(t,"deg)")})})});
--- javascript/showLarge.js
+++ javascript/showLarge.js
@@ -1,12 +1,1 @@
-function showLarge(e) {
-    document.getElementById("large_img").src = e.target.src
-}
-
-function init() {
-    let imgs = document.querySelectorAll(".small_img");
-    let i, j
-    for (let i = 0; i < imgs.length; i++) {
-        imgs[i].onclick = showLarge;
-    }
-}
-window.addEventListener("load", init, false);
+"use strict";function showLarge(e){document.getElementById("large_img").src=e.target.src}function init(){for(var e=document.querySelectorAll(".small_img"),t=0;t<e.length;t++)e[t].onclick=showLarge}window.addEventListener("load",init,!1);
